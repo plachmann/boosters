beb3e7a036697484eca1ac849ddd9f3555fceb6c
Phil Lachmann <phil.lachmann@gmail.com>
2017-07-28 13:10:36 -0400

    initial app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
